84207967da341116dc62a0f1bc56c30711a9f732
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-09 08:56:01 +0600

    Get method used with single id passed

--- src/server.ts
+++ src/server.ts
@@ -182,6 +182,38 @@ app.get("/api/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+// app.get("/api/users/:id", async (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   try {
+//     const result = await pool.query(
+//       `
+//       SELECT * FROM users WHERE id=$1  
+//         `,
+//       [id],
+//     );
+
+//     if (result.rows.length === 0) {
+//       res.status(404).json({
+//         success: false,
+//         message: "User Not found!",
+//         data: {},
+//       });
+//     }
+
+//     res.status(200).json({
+//       success: true,
+//       message: "User retrived successfully!",
+//       data: result.rows[0],
+//     });
+//   } catch (error: any) {
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//       error: error,
+//     });
+//   }
+// });
+
 app.put("/api/users/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   const { name, password, age, is_active } = req.body;
