d17ea5b090da061d73c0bc1d8ed6942f47b806c4
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-11 21:54:38 +0600

    app with cookie-parse, cors added

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import CookieParser from "cookie-parser";
+import cors from "cors";
+import express, {
+    type Application,
+    type Request,
+    type Response,
+} from "express";
+import logger from "./middleware/logger";
+import { authRoute } from "./modules/auth/auth.route";
+import { profileRoute } from "./modules/profile/profile.route";
+import { userRoute } from "./modules/user/user.route";
+import globalErrorHandler from "./middleware/globalErrorHandler";
+const app: Application = express();
+
+app.use(CookieParser());
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger);
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    }),
+);
+
+app.get("/", (req: Request, res: Response) => {
+    //res.send("Hello World!");
+    res.status(200).json({
+        message: "Express Server",
+        author: "Next Level",
+    });
+});
+
+app.use("/api/users", userRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/auth", authRoute);
+
+// Global Error Handling Middleware
+app.use(globalErrorHandler);
+
+export default app;
